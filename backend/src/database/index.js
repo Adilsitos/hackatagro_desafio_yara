fff43a42020a2e11288076f89bebaa4a5db4f96b
ext.adilson.junior <adilsonfrankneiajr@digitalsys.com.br>
2021-10-03 15:26:13 -0300

    add .env

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-mongoose.connect('mongodb://localhost/hackatagro', {useMongoClient:true});
+mongoose.connect(process.env.uri, {useMongoClient:true});
 mongoose.Promise= global.Promise
 
 module.exports= mongoose;
